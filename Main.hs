bd9d90dc3e05f95d2bf71d37e79ecc95286f2de4
Michael Rose <elementation@gmail.com>
2012-04-17 01:40:23 -0600

    Reformatted & added URL expansion

--- Main.hs
+++ Main.hs
@@ -1,6 +1,6 @@
 module Main
-    ( main
-    ) where
+        ( main
+        ) where
 
 import Data.List
 import Network
@@ -24,49 +24,49 @@ type Net = ReaderT Bot IO
 main :: IO ()
 main = bracket connect disconnect loop
   where
-    disconnect = hClose . socket
-    loop st    = catch (runReaderT run st) (\(SomeException _) -> return ())
+        disconnect = hClose . socket
+        loop st    = catch (runReaderT run st) (\(SomeException _) -> return ())
  
 
 connect = notify $ do
-    h <- connectTo server (PortNumber (fromIntegral port))
-    hSetBuffering h NoBuffering
-    return (Bot h)
+        h <- connectTo server (PortNumber (fromIntegral port))
+        hSetBuffering h NoBuffering
+        return (Bot h)
   where
-    notify a = bracket_
-        (printf "Connecting to %s ... " server >> hFlush stdout)
-        (putStrLn "done.")
-        a
+        notify a = bracket_
+                (printf "Connecting to %s ... " server >> hFlush stdout)
+                (putStrLn "done.")
+                a
 
 run :: Net ()
 run = do
-    write "NICK" nick
-    write "USER" (nick++" 0 * :raven's bot")
-    write "JOIN" channel
-    asks socket >>= listen
+        write "NICK" nick
+        write "USER" (nick++" 0 * :raven's bot")
+        write "JOIN" channel
+        asks socket >>= listen
 
 write :: String -> String -> Net ()
 write s t = do
-	h <- asks socket
-	io $ hPrintf h "%s %s\r\n" s t
-	io $ printf "> %s %s\r\n" s t
+        h <- asks socket
+        io $ hPrintf h "%s %s\r\n" s t
+        io $ printf "> %s %s\r\n" s t
 
 listen :: Handle -> Net ()
 listen h = forever $ do
-		s <- init `fmap` io (hGetLine h)
-		io $ putStrLn s
-		if ping s then pong s else eval (clean s)
-	where
-		forever a = a >> forever a
-		ping x = "PING :" `isPrefixOf` x
-		pong x = write "PONG" (':' : drop 6 x)
-		clean = drop 1 . dropWhile (/= ':') . drop 1
+                s <- init `fmap` io (hGetLine h)
+                io $ putStrLn s
+                if ping s then pong s else eval (clean s)
+        where
+                forever a = a >> forever a
+                ping x = "PING :" `isPrefixOf` x
+                pong x = write "PONG" (':' : drop 6 x)
+                clean = drop 1 . dropWhile (/= ':') . drop 1
 
 eval :: String -> Net ()
-eval 		"!quit"				        = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
-eval x 		| "!id " `isPrefixOf` x	    = privmsg (drop 4 x)
-eval x      | "!init" `isPrefixOf` x    = privmsg $ someFunction (drop 4 x)
-eval _								= return () -- ignore
+eval            "!quit"                         = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval x          | "!id " `isPrefixOf` x         = privmsg (drop 4 x)
+eval x          | "!ex" `isPrefixOf` x          = (io $ expandUrlEasy (drop 4 x)) >>= privmsg 
+eval _                                          = return () -- ignore
 
 privmsg :: String -> Net ()
 privmsg s = write "PRIVMSG" $ channel ++ " :" ++ s
--- UrlExtender.hs
+++ UrlExtender.hs
@@ -1,5 +1,34 @@
+module UrlExtender (expandUrlEasy) where
 
-module UrlExtender (someFunction) where
+import Network.HTTP
+import System.IO
+import Data.Maybe
+import Network.URI
+import Text.XML.Light
 
-someFunction :: String -> String
-someFunction f = init f
+expandUrlEasy :: String -> IO (String)
+expandUrlEasy url =
+        do resp <- expandUrl url
+           case resp of
+                Left x -> return $ "Error connecting: " ++ show x
+                Right x -> return $ url ++ " -> " ++ longurl
+                        where
+                                xml = fromJust $ parseXMLDoc x
+                                items = findElements (QName "long-url" Nothing Nothing) xml
+                                longurl = (strContent (head items))
+
+expandUrl :: String -> IO (Either String String)
+expandUrl url =
+    do resp <- simpleHTTP request
+       case resp of
+            Left x -> return $ Left ("Error connecting: " ++ show x)
+            Right r ->
+                  case rspCode r of
+                    (2,_,_) -> return $ Right (rspBody r)
+                    _ -> return $ Left (show r)
+    where 
+        request = Request {rqURI = uri,
+                            rqMethod = GET,
+                            rqHeaders = [],
+                            rqBody = ""}
+        uri = fromJust $ parseURI $ "http://api.longurl.org/v2/expand?url=" ++ (urlEncode url)
